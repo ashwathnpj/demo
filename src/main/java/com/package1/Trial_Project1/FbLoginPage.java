4c734adfcad85fac161029f198030bc421f5a19c
ashwathnpj <ashwathnpj6@gmail.com>
2024-09-20 15:17:56 +0530

    codes

---
+++ src/main/java/com/package1/Trial_Project1/FbLoginPage.java
@@ -0,0 +1,38 @@
+package com.package1.Trial_Project1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FbLoginPage {
+	// step 1 : locating each elements using @FindBy
+	WebDriver driver;
+
+	@FindBy(id = "email")
+	WebElement username;
+
+	@FindBy(id = "pass")
+	WebElement password;
+
+	@FindBy(name = "login")
+	WebElement login_button;
+
+	// step 2 : creating separate methods for each components
+	public void un() {
+		username.sendKeys("demo123@gmail.com");
+	}
+
+	public void pw() {
+		password.sendKeys("1234@demo");
+	}
+
+	public void login() {
+		login_button.click();
+	}
+
+	// step 3 : initializing the elements
+	public FbLoginPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+}
---
+++ src/test/java/com/package1/Trial_Project1/FbLogin.java
@@ -0,0 +1,28 @@
+package com.package1.Trial_Project1;
+
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FbLogin {
+	@Test
+	public void login_to_facebook() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		FbLoginPage fb = new FbLoginPage(driver);
+		fb.un();
+		fb.pw();
+		fb.login();
+		WebElement e1 = driver.findElement(By.xpath("//div[@class='pam uiBoxGray']"));
+		Assert.assertEquals(e1.isDisplayed(), true);
+	}
+}
